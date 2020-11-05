e8a2eedb2f642db1ffd3abd5a3bd99c4e3ef2228
fh <sxyzcfh@foxmail.com>
2020-11-05 15:38:19 +0800

    test the effort of multiple thread query

--- p1/src/test/java/org/example/s7/device/S7BusDeviceTest.java
+++ p1/src/test/java/org/example/s7/device/S7BusDeviceTest.java
@@ -89,6 +89,55 @@ public class S7BusDeviceTest {
         }
     }
 
+    /*
+Thread:[main][1604561676455] address:[DB1.DBB0], result: [1]
+Thread:[main][1604561676462] address:[DB1.DBB1], result: [1]
+Thread:[main][1604561676467] address:[DB1.DBB2], result: [-1]
+Thread:[main][1604561676471] address:[DB1.DBB3], result: [-3]
+Thread:[main][1604561676476] address:[DB1.DBB4], result: [-1]
+Thread:[main][1604561676480] address:[DB1.DBB5], result: [-1]
+Thread:[main][1604561676485] address:[DB1.DBB6], result: [-128]
+Thread:[main][1604561676490] address:[DB1.DBB7], result: [0]
+Thread:[main][1604561676494] address:[DB1.DBB8], result: [0]
+Thread:[main][1604561676499] address:[DB1.DBB9], result: [0]
+Thread:[main][1604561676503] address:[DB1.DBB10], result: [0]
+Thread:[main][1604561676508] address:[DB1.DBB11], result: [0]
+Thread:[main][1604561676514] address:[DB1.DBB12], result: [7]
+Thread:[main][1604561676521] address:[DB1.DBB13], result: [-48]
+Thread:[main][1604561676526] address:[DB1.DBB14], result: [0]
+Thread:[main][1604561676531] address:[DB1.DBB15], result: [0]
+Thread:[main][1604561676536] address:[DB1.DBB16], result: [11]
+Thread:[main][1604561676541] address:[DB1.DBB17], result: [-72]
+Thread:[main][1604561676546] address:[DB1.DBB18], result: [1]
+Thread:[main][1604561676550] address:[DB1.DBB19], result: [100]
+Thread:[main][1604561676554] address:[DB1.DBB20], result: [101]
+Thread:[main][1604561676559] address:[DB1.DBB21], result: [102]
+126ms
+
+Thread:[Thread-0][1604561871668] address:[DB1.DBB0], result: [1]
+Thread:[Thread-1][1604561871668] address:[DB1.DBB10], result: [0]
+Thread:[Thread-0][1604561871674] address:[DB1.DBB1], result: [1]
+Thread:[Thread-1][1604561871679] address:[DB1.DBB11], result: [0]
+Thread:[Thread-0][1604561871683] address:[DB1.DBB2], result: [-1]
+Thread:[Thread-1][1604561871688] address:[DB1.DBB12], result: [7]
+Thread:[Thread-0][1604561871693] address:[DB1.DBB3], result: [-3]
+Thread:[Thread-1][1604561871697] address:[DB1.DBB13], result: [-48]
+Thread:[Thread-0][1604561871703] address:[DB1.DBB4], result: [-1]
+Thread:[Thread-1][1604561871707] address:[DB1.DBB14], result: [0]
+Thread:[Thread-0][1604561871713] address:[DB1.DBB5], result: [-1]
+Thread:[Thread-1][1604561871717] address:[DB1.DBB15], result: [0]
+Thread:[Thread-0][1604561871721] address:[DB1.DBB6], result: [-128]
+Thread:[Thread-1][1604561871726] address:[DB1.DBB16], result: [11]
+Thread:[Thread-1][1604561871737] address:[DB1.DBB17], result: [-72]
+Thread:[Thread-0][1604561871732] address:[DB1.DBB7], result: [0]
+Thread:[Thread-1][1604561871741] address:[DB1.DBB18], result: [1]
+Thread:[Thread-0][1604561871745] address:[DB1.DBB8], result: [0]
+Thread:[Thread-1][1604561871750] address:[DB1.DBB19], result: [100]
+Thread:[Thread-0][1604561871756] address:[DB1.DBB9], result: [0]
+Thread:[Thread-1][1604561871761] address:[DB1.DBB20], result: [101]
+Thread:[Thread-1][1604561871772] address:[DB1.DBB21], result: [102]
+程序结束！耗时: 108 ms
+     */
     @Test
     public void threadTest() throws InterruptedException {
         String[] address = new String[]{
